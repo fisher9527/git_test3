41dd5690698d0f98d9adc835b167a25e1512ea7d
fisher9527 <hdheyou@163.com>
2018-02-08 09:32:44 +0800

    新增Test类和修改MainActivity.

---
+++ git_test3/app/src/main/java/com/hy/git/git_test3/Test2.java
@@ -0,0 +1,18 @@
+package com.hy.git.git_test3;
+
+/**
+ * <pre>
+ *     author : Fisher
+ *     e-mail : hdheyou@126.com
+ *     time   : 2018/02/08
+ *     version: 1.0
+ *     desc   :
+ * </pre>
+ */
+
+
+public class Test2 {
+	public static void print(){
+
+	}
+}
