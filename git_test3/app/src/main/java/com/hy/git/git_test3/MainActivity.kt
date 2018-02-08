05d0613f3df11a2ceae150a0fcad92b9fb18b62f
fisher9527 <hdheyou@163.com>
2018-02-08 08:35:53 +0800

    创建项目。

---
+++ git_test3/app/src/main/java/com/hy/git/git_test3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hy.git.git_test3
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
